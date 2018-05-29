9d7a6ac3b32558274861248bf06ba494b3fa8c2a
bdix <bdix@airmail.cc>
2018-05-28 21:22:24 -0700

    fixed merge sort being broken

--- src/sortingpoints/SortingMain.java
+++ src/sortingpoints/SortingMain.java
@@ -91,10 +91,10 @@ public class SortingMain {
 
 
        System.out.println(Arrays.toString(pointArray));
-      // insertionSort(pointArray);
-       selectionSort(pointArray);
+       //insertionSort(pointArray);
+       //selectionSort(pointArray);
        //heapSort(pointArray);
-      //mergeSort(pointArray);
+       mergeSort(pointArray);
        System.out.println(Arrays.toString(pointArray));
       
       for(int i = pointArray.length - 1; i >= 0; i--) {
@@ -163,43 +163,42 @@ public class SortingMain {
    }
    
    public static void mergeSort(Point3D[] points) {
-       Point3D[] pointsSorted = new Point3D[points.length];
-       mergeSort(points, pointsSorted, 0, points.length - 1);
+       mergeSort(points, 0, points.length - 1);
    }
 
-   private static void mergeSort(Point3D[] points, Point3D[] pointsSorted, int startIndex, int endIndex) {
+   private static void mergeSort(Point3D[] points, int startIndex, int endIndex) {
        int range = endIndex - startIndex;
+       Point3D[] sortedPoints = new Point3D[points.length];
        if (range > 50) {
-           mergeSort(points, pointsSorted, startIndex, startIndex + range / 2);
-           mergeSort(points, pointsSorted, (startIndex + range / 2) + 1, endIndex);
+           mergeSort(points, startIndex, startIndex + range / 2);
+           mergeSort(points, (startIndex + range / 2) + 1, endIndex);
            int u = startIndex;
            int v = startIndex + range / 2;
            int place = startIndex;
            while (u < startIndex + range / 2 || v <= endIndex) {
-               if (v > endIndex && u < startIndex + range / 2 || pointsSorted[u].compareTo(pointsSorted[v]) < 0 && u < startIndex + range / 2) {
-                   points[place] = pointsSorted[u];
+               if (v > endIndex && u < startIndex + range / 2 ||  points[u].compareTo(points[v]) < 0 && u < startIndex + range / 2) {
+                   sortedPoints[place] = points[u];
                    u++;
                    place++;
-               } else if (v <= endIndex) {
-                   points[place] = pointsSorted[v];
+               } else {
+                   sortedPoints[place] = points[v];
                    v++;
                    place++;
                }
            }
        } else {
-           smallSort(points, pointsSorted, startIndex, endIndex);
+           smallSort(points, startIndex, endIndex);
        }
    }
 
-   private static void smallSort(Point3D[] points, Point3D[] pointsSorted, int startIndex, int endIndex) {
+   private static void smallSort(Point3D[] points, int startIndex, int endIndex) {
        for (int i = startIndex; i <= endIndex; i++) {
-           int least = startIndex;
+           int least = i;
            for (int u = i ; u <= endIndex ; u++) {
                if (points[u].compareTo(points[least]) < 0) {
                    least = u;
                }
            }
-           pointsSorted[i] = points[least];
            Point3D hold = points[i];
            points[i] = points[least];
            points[least] = hold;
